540db1166d4936c0aa3418a77cdd453ff2b39438
HonmaMeiko <17mwang2@stu.edu.cn>
2019-06-11 10:39:53 +0800

    为了适配nginx而修改路由

--- app/router.js
+++ app/router.js
@@ -8,15 +8,15 @@ module.exports = app => {
   const loginStateAuth = app.middleware.loginStateAuth()
   router.get('/', controller.home.index)
   // 注册以及登录页面
-  router.post('/register', controller.user.user.register)
-  router.post('/login', controller.user.user.login)
+  router.post('/api/register', controller.user.user.register)
+  router.post('/api/login', controller.user.user.login)
   // 审核页面
-  router.get('/getReview', loginStateAuth, controller.user.user.getReview)
-  router.get('/reviewConfirm', loginStateAuth, controller.user.user.confirmReview)
+  router.get('/api/getReview', loginStateAuth, controller.user.user.getReview)
+  router.get('/api/reviewConfirm', loginStateAuth, controller.user.user.confirmReview)
   // 家族树渲染
-  router.get('/getTree', loginStateAuth, controller.user.user.getTree)
+  router.get('/api/getTree', loginStateAuth, controller.user.user.getTree)
   // 后台管理界面
   // todo 鉴权暂时交由前端处理，用户登陆后后端返回该用户的permission给前端
-  router.get('/insertByAdmin', loginStateAuth, controller.user.user.insertByAdmin)
-  router.get('/deleteByAdmin', loginStateAuth, controller.user.user.deleteByAdmin)
+  router.get('/api/insertByAdmin', loginStateAuth, controller.user.user.insertByAdmin)
+  router.get('/api/deleteByAdmin', loginStateAuth, controller.user.user.deleteByAdmin)
 }
